f567723b0eee335e8ff478f010bcf336292e1b22
edilsonmatos01 <edilsonmatos01@gmail.com>
2025-06-20 16:49:23 -0300

    Corrigir configuração de resolução de módulos

--- next.config.js
+++ next.config.js
@@ -3,7 +3,14 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true
   },
-  output: 'standalone'
+  output: 'standalone',
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@/components': './components'
+    };
+    return config;
+  }
 };
 
 module.exports = nextConfig; 
