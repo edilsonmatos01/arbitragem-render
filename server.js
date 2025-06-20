b92832f48add0fc8c77d1f0da0eae34df17b7fe0
edilsonmatos01 <edilsonmatos01@gmail.com>
2025-06-20 18:37:19 -0300

    fix: ajusta configuração do servidor e adiciona script build:tracker

--- server.js
+++ server.js
@@ -5,16 +5,25 @@ const WebSocket = require('ws');
 require('dotenv').config();
 
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
+const hostname = 'localhost';
+const port = process.env.PORT || 10000;
+
+const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 
 // Importa as funções do monitor
 const { startMonitor, broadcast } = require('./monitor');
 
 app.prepare().then(() => {
-  const server = createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
+  const server = createServer(async (req, res) => {
+    try {
+      const parsedUrl = parse(req.url, true);
+      await handle(req, res, parsedUrl);
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err);
+      res.statusCode = 500;
+      res.end('Internal Server Error');
+    }
   });
 
   // Configurar WebSocket Server
@@ -31,7 +40,6 @@ app.prepare().then(() => {
   // Inicia o monitor com o servidor WebSocket
   startMonitor(wss);
 
-  const port = process.env.PORT || 10000;
   server.listen(port, (err) => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
